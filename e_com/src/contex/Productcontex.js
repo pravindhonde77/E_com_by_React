368c58e1037cab5d89108d226be44eb03120fcdb
pravindhonde77 <pravindhonde77@gmail.com>
2022-11-28 17:56:17 +0530

    final

--- e_com/src/contex/Productcontex.js
+++ e_com/src/contex/Productcontex.js
@@ -1,17 +1,33 @@
 // create a  context 
 // provider
 // consumer=useContex
-import { createContext } from "react"
+import { createContext, useEffect } from "react"
+import axios from "axios"
 
 const AppContext = createContext()
+const API="https://api.pujakaitem.com/api/products"
 
 const AppProvider = ({ children }) => {
+
+    const getProducts=async(url)=>{
+        const res=await axios.get(url);
+        const products=await res.data
+        console.log(products)
+
+
+    }
+
+    useEffect(() => {
+        getProducts(API)
+
+    }, [])
+
     return (
-        <AppContext.Provider value={{myName:"Pravin Dhonde"}}>
+        <AppContext.Provider value={{ myName: "Pravin Dhonde" }}>
             {children}
         </AppContext.Provider>
     )
 
 }
 
-export { AppProvider,AppContext  }
+export { AppProvider, AppContext }
