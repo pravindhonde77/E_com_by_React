1070bf371dbe055d56dfc72f287946c62f37db3a
pravindhonde77 <pravindhonde77@gmail.com>
2022-11-28 16:28:08 +0530

    final

--- e_com/src/contex/Productcontex.js
+++ e_com/src/contex/Productcontex.js
@@ -1,1 +1,17 @@
-create
+// create a  context 
+// provider
+// consumer=useContex
+import { createContext } from "react"
+
+const AppContext = createContext()
+
+const AppProvider = ({ children }) => {
+    return (
+        <AppContext.Provider value={{myName:"Pravin Dhonde"}}>
+            {children}
+        </AppContext.Provider>
+    )
+
+}
+
+export { AppProvider,AppContext  }
