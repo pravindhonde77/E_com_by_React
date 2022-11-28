4cb26a5359d49fed9b64d315dcdc68601d7deabe
pravindhonde77 <pravindhonde77@gmail.com>
2022-11-28 11:08:57 +0530

    final

--- e_com/src/pages/Error.js
+++ e_com/src/pages/Error.js
@@ -1,9 +1,45 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import styled from 'styled-components'
+import { Button } from '../styles/Button'
 
 const Error = () => {
   return (
-    <div>Error</div>
+    <Wrapper>
+      <div className="container">
+        <div>
+          <h2>404</h2>
+          <h2>UH OH! You're lost.</h2>
+          <p>
+            The page you are lokking for does not exist.
+            how you got here is a mystery.But you can click
+            the button below to go back to the homepage.
+          </p>
+
+          <NavLink to="/">
+            <Button>Go Back To Home</Button>
+          </NavLink>
+
+        </div>
+      </div>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.section`
+  .container {
+    padding: 9rem 0;
+    text-align: center;
+    h2 {
+      font-size: 10rem;
+    }
+    h3 {
+      font-size: 4.2rem;
+    }
+    p {
+      margin: 2rem 0;
+    }
+  }
+`;
 
 export default Error
