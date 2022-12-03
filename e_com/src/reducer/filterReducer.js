41f20444dc6b8210ef5b7d6b5689fefcfda5c748
pravindhonde77 <pravindhonde77@gmail.com>
2022-12-03 15:37:57 +0530

    final

--- e_com/src/reducer/filterReducer.js
+++ e_com/src/reducer/filterReducer.js
@@ -34,9 +34,28 @@ const filterReducer = (state, action) => {
             let newSortData;
             let tempSortProduct=[...action.payload]
 
+            if(state.sorting_value==="lowest"){
+                const sortingProducts=(a,b)=>{
+                  return a.price-b.price 
+                }
+                newSortData=tempSortProduct.sort(sortingProducts)
+            }
+
+            if(state.sorting_value==="highest"){
+                const sortingProducts=(a,b)=>{
+                  return a.price-b.price 
+                }
+                newSortData=tempSortProduct.sort(sortingProducts)
+            }
+
             if(state.sorting_value==="a-z"){
                 newSortData=tempSortProduct.sort((a,b)=>{
                    return a.name.localeCompare(b.name);
+                })
+            }
+            if(state.sorting_value==="z-a"){
+                newSortData=tempSortProduct.sort((a,b)=>{
+                   return b.name.localeCompare(a.name);
                 })
             }
             return {
